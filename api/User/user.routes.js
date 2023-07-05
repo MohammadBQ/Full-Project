35c6cb9f162d75c5945e0f198f568c91b81fb0d8
mohammadbq <iiprxh@gmail.com>
2023-07-05 19:54:27 +0300

    see what happened

--- api/User/user.routes.js
+++ api/User/user.routes.js
@@ -1,36 +1,36 @@
 const express = require("express");
 const {
-  getTemp,
-  createTemp,
-  updateTemp,
-  deleteTemp,
-  fetchTemp,
+  getUser,
+  deleteUser,
+  fetchUser,
   signin,
-} = require("./temp.controllers");
+  signup,
+} = require("./user.controllers");
 const router = express.Router();
 const passport = require("passport");
 
 // Everything with the word temp is a placeholder that you'll change in accordance with your project
 
-router.param("tempId", async (req, res, next, tempId) => {
+router.param("userId", async (req, res, next, userId) => {
   try {
-    const foundTemp = await fetchTemp(tempId);
-    if (!foundTemp) return next({ status: 404, message: "Temp not found" });
-    req.temp = foundTemp;
+    const foundUser = await fetchUser(userId);
+    if (!foundUser) return next({ status: 404, message: "Temp not found" });
+    req.temp = foundUser;
     next();
   } catch (error) {
     return next(error);
   }
 });
 
-router.get("/", passport.authenticate("jwt", { session: false }), getTemp);
+router.get("/", passport.authenticate("jwt", { session: false }), getUser);
 router.post("/createTemp", createTemp);
 router.post(
   "/signin",
   passport.authenticate("local", { session: false }),
   signin
 );
+router.post("/signup",  signup);
 router.put("/:tempId", updateTemp);
-router.delete("/:tempId", deleteTemp);
+router.delete("/:userId", deleteUser)
 
 module.exports = router;
--- models/User.js
+++ models/User.js
@@ -1,11 +1,13 @@
 const { model, Schema } = require("mongoose");
 // Everything with the word temp is a placeholder that you'll change in accordance with your project
 
-const Temp2Schema = new Schema({
+const UserSchema = new Schema({
   username: { type: String, unique: true, required: true },
+  email:{type: String },
   password: { type: String, required: true },
+  confirmpassword: {type: String, required: true},
 
   // create relations in here and in the other model
 });
 
-module.exports = model("Temp2", Temp2Schema);
+module.exports = model("User", UserSchema);
