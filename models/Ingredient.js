d11132a7778f0214601c4c848ceb1d52c68dbdc1
Abdullah Fahad <iamabdullahkw@gmail.com>
2023-07-09 23:20:50 +0300

    confirm pass added

--- models/Ingredient.js
+++ models/Ingredient.js
@@ -3,10 +3,9 @@ const { model, Schema } = require("mongoose");
 const IngredientSchema = new Schema({
   name: { type: String, unique: true, required: true },
   categories: [
-    { type: Schema.Types.objectId, required: true, ref: "Category" },
+    { type: Schema.Types.ObjectId, required: true, ref: "Category" },
   ],
-  recipes: [{ type: Schema.Types.objectId, required: true, ref: "Recipe" }],
+  recipes: [{ type: Schema.Types.ObjectId, required: true, ref: "Recipe" }],
 });
 
 module.exports = model("Ingredient", IngredientSchema);
-
