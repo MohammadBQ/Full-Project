23fe4a52ad8b5e87a6359b4188b9e9f58b9952d5
mohammadbq <iiprxh@gmail.com>
2023-07-11 23:36:35 +0300

    continuing

--- models/User.js
+++ models/User.js
@@ -1,11 +1,15 @@
 const { model, Schema } = require("mongoose");
 
+
 const UserSchema = new Schema({
   username: { type: String, unique: true, required: true },
   email: { type: String },
   password: { type: String, required: true },
 
+ 
+
   recipes: [{ type: Schema.Types.ObjectId, ref: "Recipe" }],
 });
+
 
 module.exports = model("User", UserSchema);
